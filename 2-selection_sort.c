ca6278b1a72da6b2196eabe04a0c65890899221b
Luamix <luizalfonso611@gmail.com>
2024-01-29 18:05:38 -0500

    taks 3 vol 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,14 +19,11 @@ void selection_sort(int *array, size_t size)
             {
                 min_idx = j;
             } 
-            if(min_idx != i)
-            {
             swap(&array[min_idx], &array[i]);
             print_array(array, size);
-            } 
         } 
     } 
-}
+} 
 
 void swap(int *a, int *b)
 {
