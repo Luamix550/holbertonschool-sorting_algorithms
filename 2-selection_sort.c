35f48b01d5193b6ad0e93c5e3aeb8bb7c6a8c3f1
Luamix <luizalfonso611@gmail.com>
2024-01-29 18:02:50 -0500

    taks 3

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * selection_sort - selects the smallest element from an unsorted list in each iteration 
+ *  and places that element at the beginning of the unsorted list.
+ * @array: 
+ * @size:
+*/
+void selection_sort(int *array, size_t size)
+{
+
+    size_t i, j, min_idx; 
+
+    for (i = 0; i < size - 1; i++) 
+    { 
+        min_idx = i; 
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx]) 
+            {
+                min_idx = j;
+            } 
+            if(min_idx != i)
+            {
+            swap(&array[min_idx], &array[i]);
+            print_array(array, size);
+            } 
+        } 
+    } 
+}
+
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
